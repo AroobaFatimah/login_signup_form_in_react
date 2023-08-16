8bb3a836b1739ba3ab8617cbe0e63bc59afe885d
AroobaFatimah <aroobafatimaa@gmail.com>
2023-08-16 01:16:44 -0700

    added login and signup form

---
+++ src/components/signup.js
@@ -0,0 +1,10 @@
+let signup = () => {
+  return (
+    <>
+      <label>Username: </label>;
+      <input type="text" placeholder="Enter username..."></input>
+    </>
+  );
+};
+
+export default signup;
